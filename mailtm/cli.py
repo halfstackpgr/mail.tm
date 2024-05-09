8d8219df5d1f3ed7dfc06b174e0601bc37d4c548
unknown <halfstackpgr@gmail.com>
2024-05-09 11:21:14 +0530

    Done

---
+++ mailtm/cli.py
@@ -0,0 +1,14 @@
+from colorama import Fore
+def version()->None:
+    print(f'''
+            {Fore.CYAN}Mail.tm{Fore.RESET}
+        Stack Development Kit (Python)
+
+Developed by: GitHub: halfstackpgr
+
+Current Version: 0.1.0
+
+For usage, visit: https://www.github.com/halfstackpgr/Mail.tm
+''')
+    
+version()
